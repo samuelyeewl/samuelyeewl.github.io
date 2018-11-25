a5869f2e46e157b4a35c56edd4edf0a4aa369edc
Samuel Yee <samuelyeewl@gmail.com>
2018-11-25 02:37:48 -0500

    Added planetary obliquities to Research and updated cloudinary upload script

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,134 +6,133 @@ module.exports = function(grunt) {
 		cloudinary: {
 			upload: {
 				files: [{
-						expand: true,
-						cwd: "images/",
-						src: "**/*.jpg",
+					expand: true,
+					src: "images/**/*.jpg"
 				}]
-			}
+			},
 		},
 
 		// image_resize: {
 		// 	// Gallery
-		// 	gallery_thumbs: {
-		// 		options: {
-		// 			width: 144,
-		// 			height: 144,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/albums/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/albums/",
-		// 			rename: function(dest, src) {
-		// 				return dest + src.replace('.jpg', '_thumb.jpg');
-		// 			}
-		// 		}]
-		// 	},
-		// 	gallery: {
-		// 		options: {
-		// 			width: 850,
-		// 			height: 600,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/albums/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/albums/"
-		// 		}]
-		// 	},
+			// gallery_thumbs: {
+			// 	options: {
+			// 		width: 144,
+			// 		height: 144,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/albums/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/albums/",
+			// 		rename: function(dest, src) {
+			// 			return dest + src.replace('.jpg', '_thumb.jpg');
+			// 		}
+			// 	}]
+			// },
+			// gallery: {
+			// 	options: {
+			// 		width: 850,
+			// 		height: 600,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/albums/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/albums/"
+			// 	}]
+			// },
 
-		// 	// Stories
-		// 	story_retina: {
-		// 		options: {
-		// 			width: 2880,
-		// 			height: 2880,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/stories/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/stories/",
-		// 			rename: function(dest, src) {
-		// 				return dest + src.replace('.jpg', '_2880.jpg');
-		// 			}
-		// 		}]
-		// 	},
-		// 	story_full: {
-		// 		options: {
-		// 			width: 1920,
-		// 			height: 1920,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/stories/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/stories/",
-		// 			rename: function(dest, src) {
-		// 				return dest + src.replace('.jpg', '_1920.jpg');
-		// 			}
-		// 		}]
-		// 	},
-		// 	story_medium: {
-		// 		options: {
-		// 			width: 1366,
-		// 			height: 1366,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/stories/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/stories/",
-		// 			rename: function(dest, src) {
-		// 				return dest + src.replace('.jpg', '_1366.jpg');
-		// 			}
-		// 		}]
-		// 	},
-		// 	story_small: {
-		// 		options: {
-		// 			width: 850,
-		// 			height: 850,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/stories/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/stories/",
-		// 			rename: function(dest, src) {
-		// 				return dest + src.replace('.jpg', '_850.jpg');
-		// 			}
-		// 		}]
-		// 	},
-		// 	story_mobile: {
-		// 		options: {
-		// 			width: 420,
-		// 			height: 420,
-		// 			overwrite: true
-		// 		},
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: "images/stories/",
-		// 			src: "**/*.jpg",
-		// 			dest: "images/_generated/stories/",
-		// 			rename: function(dest, src) {
-		// 				return dest + src.replace('.jpg', '_420.jpg');
-		// 			}
-		// 		}]
-		// 	},
-		// }
+			// // Stories
+			// story_retina: {
+			// 	options: {
+			// 		width: 2880,
+			// 		height: 2880,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/stories/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/stories/",
+			// 		rename: function(dest, src) {
+			// 			return dest + src.replace('.jpg', '_2880.jpg');
+			// 		}
+			// 	}]
+			// },
+			// story_full: {
+			// 	options: {
+			// 		width: 1920,
+			// 		height: 1920,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/stories/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/stories/",
+			// 		rename: function(dest, src) {
+			// 			return dest + src.replace('.jpg', '_1920.jpg');
+			// 		}
+			// 	}]
+			// },
+			// story_medium: {
+			// 	options: {
+			// 		width: 1366,
+			// 		height: 1366,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/stories/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/stories/",
+			// 		rename: function(dest, src) {
+			// 			return dest + src.replace('.jpg', '_1366.jpg');
+			// 		}
+			// 	}]
+			// },
+			// story_small: {
+			// 	options: {
+			// 		width: 850,
+			// 		height: 850,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/stories/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/stories/",
+			// 		rename: function(dest, src) {
+			// 			return dest + src.replace('.jpg', '_850.jpg');
+			// 		}
+			// 	}]
+			// },
+			// story_mobile: {
+			// 	options: {
+			// 		width: 420,
+			// 		height: 420,
+			// 		overwrite: true
+			// 	},
+			// 	files: [{
+			// 		expand: true,
+			// 		cwd: "images/stories/",
+			// 		src: "**/*.jpg",
+			// 		dest: "images/_generated/stories/",
+			// 		rename: function(dest, src) {
+			// 			return dest + src.replace('.jpg', '_420.jpg');
+			// 		}
+			// 	}]
+			// },
+	// 	}
 	});
 
 	grunt.loadNpmTasks('grunt-newer');
-	// grunt.loadNpmTasks('grunt-image-resize');
+	// grunt.loadNpmTasks('grunt-cloudinary');
+	grunt.loadNpmTasks('grunt-image-resize');
 
 	grunt.registerMultiTask('cloudinary', function() {
-		var done = this.async();
 		var cloudinary = require('cloudinary');
 		var options = this.options({
 	      account: grunt.file.readJSON('images/cloudinary-account.json')
@@ -155,6 +154,6 @@ module.exports = function(grunt) {
 	    });
 	});
 
-	// grunt.registerTask('images', ['newer:image_resize']);
-	grunt.registerTask('upload', ['newer:cloudinary']);
+	grunt.registerTask('images', ['newer:image_resize']);
+	grunt.registerTask('upload', ['newer:cloudinary:upload']);
 };
